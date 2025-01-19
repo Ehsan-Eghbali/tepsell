aad8ed442ab92659c517f2891a5d9a3f62679b7a
eghbali.ehsan <eghbali.ehsan@sabaidea.team>
2025-01-19 23:22:29 +0330

    feat: update

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 import ProfileCard from "./profilecard";
 import Personalinfo from "./Personalinfo";
+
 import ProfileTab from "./ProfileTab";
 
 
